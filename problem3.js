a4fb9395fe77bb11f2120c6d2c9c7454e229c146
Ryota Seki <ryotabs@hawaii.edu>
2018-10-06 14:52:17 -1000

    Found factors, just gotta find primes

---
+++ problem3.js
@@ -0,0 +1,27 @@
+
+/*
+ Largest prime factor
+ The prime factors of 13195 are 5, 7, 13, and 29
+ What is the largest prime factor of the number 600851475143?
+ */
+
+const testInput = 13195;
+
+function factors(input) {
+  const listFactors = [];
+  for (let i = 0; i < input; i++) {
+    if (input % i === 0) {
+      listFactors.push(i);
+    }
+  }
+  return listFactors;
+}
+
+// console.log(factors(testInput));
+
+function primeFactors(listOfFactors) {
+  for (let factor of listOfFactors) {
+  }
+}
+
+primeFactors(factors(testInput));
