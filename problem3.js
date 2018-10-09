c1dd6c6523b9da4bf6e133f0e2fb381a774eafcf
Ryota Seki <ryotabs@hawaii.edu>
2018-10-09 11:20:40 -1000

    Tried to find a method to find prime numbers, unsuccessful

--- problem3.js
+++ problem3.js
@@ -5,11 +5,11 @@
  What is the largest prime factor of the number 600851475143?
  */
 
-const testInput = 13195;
+const testInput = 600851475143;
 
 function factors(input) {
   const listFactors = [];
-  for (let i = 0; i < input; i++) {
+  for (let i = 0; i <= input; i++) {
     if (input % i === 0) {
       listFactors.push(i);
     }
@@ -20,8 +20,13 @@ function factors(input) {
 // console.log(factors(testInput));
 
 function primeFactors(listOfFactors) {
+  const listOfPrimes = [];
   for (let factor of listOfFactors) {
+    if (factors(factor).length === 2) {
+      listOfPrimes.push(factor);
+    }
   }
+  return listOfPrimes;
 }
 
-primeFactors(factors(testInput));
+console.log(primeFactors(factors(testInput)));
